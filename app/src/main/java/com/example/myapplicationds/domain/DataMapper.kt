4a8581284977846d99d49ded45793ae58ecbc8a0
odinstoodin <odinstoodin@gmail.com>
2020-11-25 15:40:16 +0200

    updated DomainData.kt, DataMapper.kt

--- app/src/main/java/com/example/myapplicationds/domain/DataMapper.kt
+++ app/src/main/java/com/example/myapplicationds/domain/DataMapper.kt
@@ -2,8 +2,15 @@ package com.example.myapplicationds.domain
 
 import com.example.myapplicationds.data.local.model.LocalData
 import com.example.myapplicationds.data.remote.model.ResponseData
+import com.example.myapplicationds.domain.model.DomainData
 
 fun ResponseData.toLocalData() = LocalData(
+    id = id,
+    title = title,
+    body = body
+)
+
+fun ResponseData.toDomainData() = DomainData(
     id = id,
     title = title,
     body = body
--- app/src/main/java/com/example/myapplicationds/domain/model/DomainData.kt
+++ app/src/main/java/com/example/myapplicationds/domain/model/DomainData.kt
@@ -1,3 +1,7 @@
 package com.example.myapplicationds.domain.model
 
-data class DomainData(val id: Int, val title: String, val body: String)
+data class DomainData(
+    val id: Int,
+    val title: String,
+    val body: String
+)
